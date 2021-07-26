d253e08deeb9e6567eab1b0b7de02ff7ac64d7f8
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-26 11:15:44 -0500

    The two layouts are already done correctly for mobile and desktop

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,49 +5,50 @@ const About = () => {
         <section class="aboutContainer">
             <h2>What is a linear equation?</h2>
             <p>An equation having the maximum order of 1 is known as a Linear equation.
-            <br>
+            <br><br>
             Below are some examples of linear equations in 1 variable, 2 variables and 3 variables:</p>
             <article>
                 <h3>Linear equations in one variable</h3>
                 <p>3x+5=0
-                <br>
+                <br><br>
                 32x+7=0
-                <br>
+                <br><br>
                 98x = 49</p>
                 <h3>Linear equations in two variables</h3>
                 <p>y+7x=3
-                <br>
+                <br><br>
                 3a+2b = 5
-                <br>
+                <br><br>
                 6x+9y-12=0</p>
                 <h3>Linear equations in three variables</h3>
                 <p>x + y + z = 0
-                <br>
+                <br><br>
                 a – 3b = c
-                <br>
+                <br><br>
                 3x + 12 y = ½ z</p>
             </article>
             <h2>How to solve linear equations?</h2>
             <p>There are many different linear equations, as well as many forms of these equations.
-            <br>
+            <br><br>
             What it is important for us is learning how to solve linear equations. In this article it will be told how to solve linear equations in one variable.</p>
             <article>
                 <h3>Solution of linear equation in one variable</h3>
                 <p>Both sides of the equation are supposed to be balanced for solving a linear equation. 
+                <br><br>
                 Equality sign denotes that the expressions on either side of the ‘equal to’ sign are equal. Since the equation is balanced, for solving it certain mathematical operations are performed on both sides of the equation in a manner that it does not affect the balance of the equation. Here is the example related to the linear equation in one variable.</p>
                 <h3>Example: Solve (2x – 10)/2 = 3(x – 1)</h3>
                 <h3>Step 1: Clear the fraction</h3>
                 <p>x – 5 = 3(x – 1)</p>
                 <h3>Step 2: Simplify Both sides of the equation</h3>
                 <p>2x – 5 = 3x – 3
-                <br>
+                <br><br>
                 x = 3x + 2
-                <br>
+                <br><br>
                 x – 3x = 2
                 </p>
                 <h3>Step 3: Isolate X</h3>
                 <p>-2x = 2
-                <br>
+                <br><br>
                 x = -1</p>
             </article>
             <h2>Why it is important to know how to solve linear equations?</h2>
