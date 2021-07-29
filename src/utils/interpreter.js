42cf826f788c2da1a22f86eddc52a35c54a5187d
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-29 18:35:28 -0500

    I have problems with divitions with big numbers below. I need to fix that

--- src/utils/interpreter.js
+++ src/utils/interpreter.js
@@ -1,25 +1,59 @@
-const div = () => {
+let arr = []
+let i = 0
+
+const div = (num1, num2) => {
     return `
     <div class="divition">
-        num1
+        ${num1}
         <hr>
-        num2
+        ${num2}
     </div>
     `
 }
 
 const getMatch = (string) => {
-    let regExp = /\d{1,}\w?\/\d{1,}/g;
-    return string.replace(regExp, div)
+    let regExp = /\d{1,}\w?\/\d+/g;
+
+    if(arr.length >= 1){
+        string = string.substring(arr[0].length - (73 * i))
+    }
+    
+    let result
+    let myMatch = string.match(regExp)
+
+    if(myMatch){
+        let numbers, subresult
+        i++
+        if(arr.length >= 1){
+            numbers = myMatch[0].split("/")
+            subresult = string.replace(regExp, div(...numbers))
+            result = `${arr[0]}${subresult}`
+            arr[0] = result
+        }else{
+            numbers = myMatch[0].split("/")
+            result = string.replace(regExp, div(...numbers))
+            arr[0] = result
+        }
+    }else{
+        if(arr.length >= 1){
+            result = `${arr[0]}${string}`
+        }else{
+            result = string
+        }
+    }
+
+    return result
 }
 
 const interpreter = () => {
     let input = document.querySelector("input")
     let interpretation = document.querySelector(".interpretation")
 
-    input.addEventListener("input", () => {
-        let str = input.value
-        interpretation.innerHTML = getMatch(str)
+    input.addEventListener("input", (evt) => {
+        if(input.value == ""){
+            arr = []
+        }
+        interpretation.innerHTML = getMatch(input.value)
     })
 }
 
