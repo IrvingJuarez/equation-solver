b1022877f00ea9f63fed15347f63266e4c51a45b
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-29 13:05:21 -0500

    I figured out a way to do what I want to do, I just going to test it to see how it works

--- src/utils/interpreter.js
+++ src/utils/interpreter.js
@@ -1,8 +1,24 @@
+const div = `<div>
+    numberOne
+    <hr>
+    numberTwo
+</div>`
+
+const getMatch = (string) => {
+    let regExp = /\d{1,}\/\d{1,}/g;
+    let matches = [...string.matchAll(regExp)]
+    let result = string.replace(regExp, div)
+
+    return result
+}
+
 const interpreter = () => {
     let input = document.querySelector("input")
     let interpretation = document.querySelector(".interpretation")
+
     input.addEventListener("input", () => {
-        interpretation.textContent = input.value
+        let str = input.value
+        interpretation.innerHTML = getMatch(str)
     })
 }
 
