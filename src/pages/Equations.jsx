110c4935ebe8d9ed58fee59d1e660c136e92fda7
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-26 15:15:20 -0500

    The Equations page is done. its left to do some stuff but its almost done. well, at least in the design

--- src/pages/Equations.jsx
+++ src/pages/Equations.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 
 const Equations = () => (
-    <h2>I am the equations page</h2>
+    <section className="equations">
+        <h2>Write a linear equation</h2>
+        <input type="text" placeholder="E.g: 2x + 10 = 23x + 120" />
+        <p>.</p>
+        <button>Do the math</button>
+    </section>
 )
 
 export default Equations
