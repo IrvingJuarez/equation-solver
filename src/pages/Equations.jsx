c0b49dc11b72ea3694e346206663b4167cde2eb8
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-26 20:56:23 -0500

    The raise of the state is done, now I have to add the regExp's to start watching magic

--- src/pages/Equations.jsx
+++ src/pages/Equations.jsx
@@ -4,10 +4,19 @@ import chosenPage from "../utils/chosenPage"
 class Equations extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            interpretation: "."
+        }
     }
 
     componentDidMount(){
         chosenPage("Equations")
+    }
+
+    handlerChange = e => {
+        this.setState({
+            interpretation: e.target.value
+        })
     }
 
     render(){
@@ -15,8 +24,8 @@ class Equations extends React.Component {
         return(
             <section className="equations">
                 <h2>Write a linear equation</h2>
-                <input type="text" placeholder="E.g: 2x + 10 = 23x + 120" />
-                <p>.</p>
+                <input type="text" placeholder="E.g: 2x + 10 = 23x + 120" onChange={this.handlerChange}/>
+                <p>{this.state.interpretation}</p>
                 <button>Do the math</button>
             </section>
         )
