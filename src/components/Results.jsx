d397845ab5e2d3c66f452fc4b792932c4e1a2ae2
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-30 09:58:19 -0500

    The Results component styles are done and now I only have to show them up when pushed the button

---
+++ src/components/Results.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Results = () => (
+    <section className="results">
+        <h2 className="results__var-result">x = 50</h2>
+
+        <article className="results__module">
+            <button>See process</button>
+        </article>
+
+        <article className="results__module">
+            <button>See comprobation</button>
+        </article>
+    </section>
+)
+
+export default Results
