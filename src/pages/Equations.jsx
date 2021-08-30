2b2cf8798026933182b0c8e1639ff9e2beb960c2
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-30 10:17:27 -0500

    The loader works, now I only have to improve it and add a try again button

--- src/pages/Equations.jsx
+++ src/pages/Equations.jsx
@@ -1,17 +1,20 @@
 import React from "react"
 import Results from "../components/Results"
+import Loader from "../components/Loader"
 
 import chosenPage from "../utils/chosenPage"
 import interpreter from "../utils/interpreter"
+import doMath from "../utils/doMath"
 
 const regExp = new RegExp("\=")
 
 class Equations extends React.Component {
     constructor(props){
         super(props)
-        this.state = { enabled: false, equation: "" }
+        this.state = { enabled: false, equation: "", loading: null, error: null }
 
         this.handlerChange = this.handlerChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount(){
@@ -21,7 +24,11 @@ class Equations extends React.Component {
     }
 
     handleClick = () => {
-        console.log(this.state.equation)
+        this.setState({
+            loading: true
+        })
+
+        this.result = doMath(this.state.equation, this)
     }
 
     handlerChange = e => {
@@ -43,7 +50,16 @@ class Equations extends React.Component {
         }else{
             return( <button className="nonAbled" disabled>Do the math</button> )
         }
+    }
 
+    renderResults = () => {
+        if(this.state.loading === null){
+            return( <div></div> )
+        }else if(this.state.loading){
+            return( <Loader />)
+        }else{
+            return( <Results />)
+        }
     }
 
     render(){
@@ -53,7 +69,7 @@ class Equations extends React.Component {
                 <input type="text" placeholder="E.g: 2x + 10 = 23x + 120" onChange={this.handlerChange}/>
                 <div className="equations__interpretation">.</div>
                 {this.renderBtn()}
-                <Results />
+                {this.renderResults()}
             </section>
         )
     }
--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -1,72 +1,9 @@
-const varSide = (side1, side2) => {
-    let vars1 = regex(side1) || []
-    let vars2 = regex(side2) || []
-    let varside
-
-    if(vars1.length === vars2.length){
-        let random = Math.floor(Math.random() * 2)
-        if(random == 0){
-            random = side1
-        }else{
-            random = side2
-        }
-        varside = `${random} was chosen randomly`
-    }else if(vars1.length > vars2.length){
-        varside = `${side1} has more variables`
-    }else{
-        varside = `${side2} has more variables`
-    }
-
-    console.log(varside)
-}
-
-const comparition = (arr, firstValue) => {
-    for(let i = 1; i < arr.length; i++){
-        if(arr[i] != firstValue){
-            return false
-        }
-    }
-
-    return true
-}
-
-const howManyVars = (arr, str) => {
-    let result, flag
-    let solvingField = document.querySelector(".result")
-
-    if(arr.length > 1){
-        flag = comparition(arr, arr[0])
-        if(flag){
-            result = 1
-        }else{
-            result = 0
-        }
-    }else{
-        result = 1
-    }
-
-    if(result){
-        let equalities = str.split("=")
-        varSide(...equalities)
-        result = `${arr[0]} = something`
-    }else{
-        result = "Linear equations have only one variable. Try again with a linear equation"
-    }
-
-    solvingField.textContent = result
-}
-
-const regex = (str) => {
-    let varRegExp = /[A-z]/g
-    return str.match(varRegExp)
-}
-
-const doMath = () => {
-    let input = document.querySelector("input")
-    let str = input.value.toLowerCase()
-    let vars = regex(str)
-
-    howManyVars(vars, str)
+const doMath = (equation, component) => {
+    setTimeout(() => {
+        component.setState({
+            loading: false
+        })
+    }, 3000)
 }
 
 export default doMath
