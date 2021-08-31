b03bd6882123140ffe5d94fec1db7e4bd8122928
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-30 19:24:52 -0500

    The first steps towards the solution of the equation

--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -1,9 +1,25 @@
+const signRegexp = /[^\+\-\*\(\)\[\]\d\s\=a-zA-Z]/g
+const letterRegex = new RegExp("[A-z]", "g");
+let signMatches, letterMatches
+
 const doMath = (equation, component) => {
-    setTimeout(() => {
+    equation = equation.toLowerCase()
+    signMatches = [...equation.matchAll(signRegexp)]
+
+    if(signMatches.length == 0){
+        letterMatches = [...equation.matchAll(letterRegex)]
+        if(letterMatches.length == 1){
+            console.log("You are right, let's pass to the next section")
+        }else{
+            component.setState({
+                error: new Error("You are using more than one variables")
+            })
+        }
+    }else{
         component.setState({
-            loading: false
+            error: new Error("You are using an invalid sign")
         })
-    }, 3000)
+    }
 }
 
 export default doMath
