fe7cb7cc8ddf12453ba0f823db776f5a4ee7be23
Irving Juarez <irvingjuarez274@gmail.com>
2021-08-31 12:56:05 -0500

    Now I know what's the side I can start with, the next step is the isolation of the variable

--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -1,5 +1,22 @@
+const regex = new RegExp("[a-z]", "ig")
+let sides, sideOneLenght, sideTwoLenght
+
+const resolveWhichSide = (equation) => {
+    sides = equation.split("=")
+    sideOneLenght = [...sides[0].matchAll(regex)]
+    sideTwoLenght = [...sides[1].matchAll(regex)]
+
+    if(sideOneLenght.length > sideTwoLenght.length){
+        console.log(sides[0])
+    }else if (sideTwoLenght.length > sideOneLenght.length){
+        console.log(sides[1])
+    }else{
+        console.log("Random")
+    }
+}
+
 const doMath = (equation, component) => {
-    console.log(equation)
+    resolveWhichSide(equation)
     component.setState({ loading: false })
 }
 
