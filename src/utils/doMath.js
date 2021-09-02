f6fe2ca985d6ecfae9a25da0f84b977ed5285164
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-02 18:59:56 -0500

    Working on the doMath script

--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -1,18 +1,29 @@
 const regex = new RegExp("[a-z]", "ig")
 let sides, sideOneLenght, sideTwoLenght
 
+const isolatingVar = (result) => {
+    
+}
+
 const resolveWhichSide = (equation) => {
+    let result
     sides = equation.split("=")
     sideOneLenght = [...sides[0].matchAll(regex)]
     sideTwoLenght = [...sides[1].matchAll(regex)]
 
     if(sideOneLenght.length > sideTwoLenght.length){
-        console.log(sides[0])
+        result = 0
     }else if (sideTwoLenght.length > sideOneLenght.length){
-        console.log(sides[1])
+        result = 1
     }else{
-        console.log("Random")
+        if(Math.floor(Math.random() * 2) == 1){
+            result = 0
+        }else{
+            result = 1
+        }
     }
+
+    isolatingVar(result)
 }
 
 const doMath = (equation, component) => {
