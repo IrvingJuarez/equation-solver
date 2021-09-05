24c6ad8dcfb06e8bdb5329d25a683b9ada9a42a1
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-05 12:25:43 -0500

    Advances in the change of the two sides of the equation. Now the side where the variable will be isolated isolates correctly

--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -1,15 +1,38 @@
-const regex = new RegExp("[a-z]", "ig")
-let sides, sideOneLenght, sideTwoLenght
+const regexVars = new RegExp("[a-z]", "ig")
+const regexIsolatedSide = /[\+\-]?\s*\d+[^a-zA-Z]/ig;
+const regexNumSide = /\d+\s*[a-z]/ig;
+let sides, sideOneLenght, sideTwoLenght, newNumberSide
+let numbersSide = 0
+
+const execNumbersSide = () => {
+
+    // console.log(sides[numbersSide])
+}
+
+const execIsolatedSide = (varSide) => {
+    newNumberSide = `${sides[numbersSide]} ${[...sides[varSide].matchAll(regexIsolatedSide)]}`
+    newNumberSide = newNumberSide.replaceAll(",", "")
+    sides[numbersSide] = newNumberSide
+    // console.log(sides[numbersSide])
+
+    sides[varSide] = sides[varSide].replaceAll(regexIsolatedSide, "")
+    // console.log(sides[varSide])
+}
 
 const isolatingVar = (result) => {
-    
+    if(result === 0){
+        numbersSide = 1
+    }
+
+    execIsolatedSide(result)
+    execNumbersSide(numbersSide)
 }
 
 const resolveWhichSide = (equation) => {
     let result
     sides = equation.split("=")
-    sideOneLenght = [...sides[0].matchAll(regex)]
-    sideTwoLenght = [...sides[1].matchAll(regex)]
+    sideOneLenght = [...sides[0].matchAll(regexVars)]
+    sideTwoLenght = [...sides[1].matchAll(regexVars)]
 
     if(sideOneLenght.length > sideTwoLenght.length){
         result = 0
