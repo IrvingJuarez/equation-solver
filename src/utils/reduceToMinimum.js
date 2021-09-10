8d26ba42615da61e9c3190fa7f97c377ed184033
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-10 13:01:18 -0500

    Right now the reducer is kinda optimized, but it only makes one round. I want it to do as many rounds as necessary in order to reduce the expression to its minimum level

---
+++ src/utils/reduceToMinimum.js
@@ -0,0 +1,33 @@
+let returnNum1, returnNum2, dividers = [10, 7, 5, 3, 2]
+
+const divisibleBy = (num) => {
+    if(returnNum1 % num === 0 && returnNum2 % num === 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+const divideBy = (num) => {
+    returnNum1 /= num
+    returnNum2 /= num
+}
+
+const reduce = () => {
+    for(let value of dividers){
+        if(divisibleBy(value)){
+            divideBy(value)
+            return null
+        }
+    }
+}
+
+const reduceToMinimum = (num1, num2) => {
+    returnNum1 = num1
+    returnNum2 = num2
+
+    reduce()
+    return `<div class="results-fraction"><p>${returnNum1}</p><hr /><p>${returnNum2}</p></div>`
+}
+
+export default reduceToMinimum
