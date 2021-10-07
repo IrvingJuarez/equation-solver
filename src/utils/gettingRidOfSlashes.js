3f0791663f9caab8e1928a61734a3bf459392565
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-07 18:19:42 -0500

    The loseSlahes will return the same number if there it any variable on it

--- src/utils/gettingRidOfSlashes.js
+++ src/utils/gettingRidOfSlashes.js
@@ -1,12 +1,26 @@
-import { sides } from "./doMath"
+import { sides, regexVars } from "./doMath"
 
 const regexSlashes = /[\+\-]\s*\d+\s*[a-z]?\s*\/\s*\d+[a-z]?/;
+
+const solveMatch = (match) => {
+    let flag2 = regexVars.test( match )
+
+    if( flag2 ){
+        return "match non-solved"
+    }else{
+        return eval( String(match) );
+    }
+}
 
 const gettingRidOfSlashes = (side) => {
     let flag = regexSlashes.test(sides[side])
 
     if(flag){
-        console.log("There is a match")
+        let matches = [...sides[side].match(regexSlashes)]
+        for(let i of matches){
+            i = solveMatch(i)
+            console.log(i)
+        }
     }
 
     return sides[side]
