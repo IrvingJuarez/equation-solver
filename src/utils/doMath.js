e4dfeedfd09696f3bc51a54e3c7f0d145fde129f
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-07 18:36:34 -0500

    Now, apparentely, the slashes were removed correctly and completely

--- src/utils/doMath.js
+++ src/utils/doMath.js
@@ -92,9 +92,13 @@ const loseSlashes = (side) => {
 }
 
 const execution = (side, c) => {
+    console.log(sides[side])
+    console.log(sides[numbersSide])
     gettingRidOfParentheses(side)
     loseSlashes(side)
-
+    
+    console.log(sides[side])
+    console.log(sides[numbersSide])
     // execute(side, regexIsolatedSide, newNumberSide, numbersSide)
     // execute(numbersSide, regexNumSide, newIsolatedSide, side, c)
 }
--- src/utils/gettingRidOfSlashes.js
+++ src/utils/gettingRidOfSlashes.js
@@ -2,11 +2,25 @@ import { sides, regexVars } from "./doMath"
 
 const regexSlashes = /[\+\-]\s*\d+\s*[a-z]?\s*\/\s*\d+[a-z]?/;
 
+const withSign = (str) => {
+    let flag3 = /\+\-/.test( str )
+
+    if( !flag3 ){
+        str = `+ ${str}`
+    }
+
+    return str
+}
+
 const solveMatch = (match) => {
     let flag2 = regexVars.test( match )
 
     if( flag2 ){
-        return "match non-solved"
+        let tempContainer = [...match.match(regexVars)]
+        match = match.replaceAll( tempContainer[0], "" )
+        match = eval( String(match) )
+        match = withSign(match)
+        return `${ match }${tempContainer[0]}`
     }else{
         return eval( String(match) );
     }
@@ -18,8 +32,7 @@ const gettingRidOfSlashes = (side) => {
     if(flag){
         let matches = [...sides[side].match(regexSlashes)]
         for(let i of matches){
-            i = solveMatch(i)
-            console.log(i)
+            sides[side] = sides[side].replace( i, solveMatch(i) )
         }
     }
 
