5f0739a710c6d1ca2adfff5ea3048a87b61a73e4
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-08 19:14:20 -0500

    The problem is apparentely solved, I need to test it as a whole yet

--- src/utils/loseWeirdDecimals.js
+++ src/utils/loseWeirdDecimals.js
@@ -1,7 +1,26 @@
 import { sides } from "./doMath"
 
+const regexWeridD = /[\+\-]\s*\d+\s*\d+\/\d+/i;
+const slashesOff = /[\+\-]?\d+\s*\/[\+\-]?\s*\d+/i;
+
+const getNormalDecimals = (str) => {
+    let slash = [...str.match(slashesOff)]
+    let normalDecimal = eval(String( slash[0] ))
+    str = str.replace(slash[0], `+ ${normalDecimal}`)
+    return eval( String(str) )
+}
+
 const loseWeirdDecimals = (side) => {
-    console.log( sides[side] )
+    let flag = regexWeridD.test( sides[side] )
+
+    if(flag){
+        let match = [...sides[side].match(regexWeridD)]
+        let rp = getNormalDecimals(match[0])
+        sides[side] = `+ ${sides[side].replace(match[0], rp)}`
+        console.log( sides[side] )
+    }
+
+    // return sides[side]
 }
 
 export default loseWeirdDecimals;
